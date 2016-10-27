c469a0fa557b22ce86f8326f7e63770dd8e70963
colophonemes <dev@colophonemes.com>
2016-10-27 19:56:32 +0100

    add ermahgerd and prompt

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const chalk = require('chalk')
-setInterval(() => {
-  const colour = ['red', 'blue', 'cyan', 'green', 'magenta', 'yellow']
-  const message = 'HEEEEELLLOOO PERTER'
-  console.log(chalk[colour.splice(Math.round(Math.random() * (colour.length - 1)), 1)](message))
-}, 300)
+const inquirer = require('inquirer')
+const ermahgerd = require('node-ermahgerd').translate
+
+const questions = [
+  {
+    name: 'name',
+    'message': 'Type your name'
+  }
+]
+
+inquirer.prompt(questions)
+  .then((answers) => {
+    setInterval(() => {
+      const colour = ['red', 'blue', 'cyan', 'green', 'magenta', 'yellow']
+      const message = ermahgerd(`oh my god! hello ${answers.name}, how are you`)
+      console.log(chalk[colour.splice(Math.round(Math.random() * (colour.length - 1)), 1)](message))
+    }, 300)
+  })
+
