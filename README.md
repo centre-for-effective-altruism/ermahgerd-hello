1318837cfa77b3cc24d39345d30cd2bbe0e66b2d
colophonemes <dev@colophonemes.com>
2016-10-27 19:59:27 +0100

    Add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# SAY IT WITH ERMAGERD <3
+
+```
+git clone https://github.com/centre-for-effective-altruism/ermahgerd-hello
+cd ermahgerd-hello
+node index
+```
--- index.js
+++ index.js
@@ -13,7 +13,7 @@ inquirer.prompt(questions)
   .then((answers) => {
     setInterval(() => {
       const colour = ['red', 'blue', 'cyan', 'green', 'magenta', 'yellow']
-      const message = ermahgerd(`oh my god! hello ${answers.name}, how are you`)
+      const message = ermahgerd(`oh my god! hello there ${answers.name}, how are you`)
       console.log(chalk[colour.splice(Math.round(Math.random() * (colour.length - 1)), 1)](message))
     }, 300)
   })
